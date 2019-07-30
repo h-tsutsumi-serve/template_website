62158f7f1d1e84e7a4524a5ce824d48460bc845d
h.tsutsumi <h.tsutsumi@serve.jp>
2019-07-30 16:06:23 +0900

    コンポーネント追加

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,4 +1,72 @@
 $(window).on('load', function () {
+
+  //navi
+  var current = false;
+  var $gBtn = $('#js-gnavMenu');
+  var $gNav = $('#js-gnav');
+  var $mainContents = $('main');
+
+  $('#js-gnav a[href^="#"]').click(function () {
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+
+    $gNav.stop().removeClass('is-enable');
+    $gBtn.removeClass('is-close');
+    current = false;
+
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
+
+  //sp
+  $gBtn.on('click', function () {
+    if (current == false) {
+      current = true;
+      if (!$(this).hasClass('is-close')) {
+        $gNav.stop().addClass('is-enable');
+        $gBtn.addClass('is-close');
+        current = false;
+      } else {
+        $gNav.stop().removeClass('is-enable');
+        $gBtn.removeClass('is-close');
+        current = false;
+      }
+    } else {
+      return false;
+    }
+  });
+
+  //pc
+  var GT = $gNav.offset().top;
+
+  function headerFixed() {
+    var ST = $(window).scrollTop();
+    var GH = $gNav.innerHeight();
+
+    if (ST > GT) {
+      $gNav.addClass('is-fixed');
+      $mainContents.css({
+        'margin-top': GH + 'px'
+      });
+
+    } else {
+      $gNav.removeClass('is-fixed');
+      $mainContents.css({
+        'margin-top': 0
+      });
+    }
+  }
+  headerFixed();
+
+  $(window).on('scroll resize', function () {
+    headerFixed();
+  });
+
+
   //pagetop
   var $pagetop = $('#js-pagetop');
   var $footer = $('footer');
@@ -12,7 +80,7 @@ $(window).on('load', function () {
     var flag = ST + WH > FT;
 
     if (flag) {
-      $pagetop.stop().css({
+      $pagetop.css({
         'display': 'block',
         'position': 'absolute',
         'bottom': FH + 15 + 'px'
@@ -24,7 +92,7 @@ $(window).on('load', function () {
           'bottom': '15px'
         }).fadeIn(200);
       } else {
-        $pagetop.stop().fadeOut(200);
+        $pagetop.fadeOut(200);
       }
     }
   }
---
+++ static/assets/js/top.js
@@ -0,0 +1,16 @@
+//swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 0,
+  loop: true,
+  speed: 2000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
